cf14490cde3ff3d79a044e92ad987d2b28da57bb
li540957517 <540957517@qq.com>
2017-09-25 19:36:45 +0800

    vcard 分页以及侧边栏功能测初步实现

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,23 @@
+from django import template
+from blog.models import Post, Category
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts():
+    """自定义简单标签"""
+    return Post.published.count()
+
+
+@register.inclusion_tag('latest_posts.html')
+def show_latest_posts(count=5):
+    """自定义包含标签"""
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.assignment_tag
+def get_all_categories():
+    """自定义赋值标签"""
+    return Category.objects.all()
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Comment, Post, Category
 
 
@@ -7,8 +8,18 @@ from .models import Comment, Post, Category
 def blog_list(request):
     """博客列表页"""
     # posts = Post.objects.filter(status='published')
-    posts = Post.published.all()
-    return render(request, 'blog-list.html', {'posts': posts})
+    # posts = Post.published.all()
+    # return render(request, 'blog-list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog-list.html', {'page': page, 'posts': posts, 'paginator': paginator})
 
 
 def blog_detail(request, year, month, day, slug):
@@ -19,4 +30,6 @@ def blog_detail(request, year, month, day, slug):
                              publish__month=month,
                              publish__day=day,
                              status='published')
+    # categories = Category.objects.all()
+    # return render(request, 'blog-detail.html', {'post': post, 'categories': categories})
     return render(request, 'blog-detail.html', {'post': post})
